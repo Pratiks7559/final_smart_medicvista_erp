f3b5430997df2275575d0dd1ca054e5124512d1e
Pratik Zope <pratikzope04@gmail.com>
2026-03-26 14:11:19 +0530

    top

--- pharmamgmt/pharmamgmt/settings.py
+++ pharmamgmt/pharmamgmt/settings.py
@@ -103,7 +103,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
